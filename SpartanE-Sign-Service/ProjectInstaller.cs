3de1c0a8d3cac21f3adf5666c28d1e32c6bd16b3
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-10-22 17:24:45 -0500

    fres
    fresh update

---
+++ SpartanE-Sign-Service/ProjectInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace SpartanE_Sign_Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAfterInstall(IDictionary savedState) {
+            // Here we going to run the program automatically
+            ServiceController sc = new ServiceController("SpartanESign2");
+            sc.Start(); // Finally start the service.
+
+        }
+
+
+
+    }
+}
---
+++ SpartanE-Sign-Service/ServiceSpartanE.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace SpartanE_Sign_Service
+{
+    public partial class ServiceSpartanE : ServiceBase
+    {
+    
+        public ServiceSpartanE()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // First Make sure the exe is not running.
+            try
+            {
+
+                onCheckProcess();
+                Process.Start(AppDomain.CurrentDomain.BaseDirectory + "ServiceFiles/microservice-letter.exe");
+                
+               
+
+            }
+            finally {
+
+               
+            }
+            
+           
+        }
+
+
+        protected override void OnStop()
+        {
+            try {
+                onCheckProcess();
+
+            } finally { }
+            
+        }
+
+        public void onCheckProcess() {
+            Process[] pList = Process.GetProcessesByName("microservice-letter");
+
+            foreach (Process p in pList)
+            {
+                
+                p.Kill();
+               
+            }
+        }
+
+    }
+}
